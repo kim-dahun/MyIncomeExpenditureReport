ddddb919ad9bdd425b46a5eced1c956ea108153c
KimDahun <117079872+kim-dahun@users.noreply.github.com>
2023-10-19 22:04:24 +0900

    Update README.md

---
+++ MyMoneyDiary/src/main/java/com/diary/money/MyMoneyDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.diary.money;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMoneyDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyMoneyDiaryApplication.class, args);
+    }
+
+}
---
+++ MyMoneyDiary/src/test/java/com/diary/money/MyMoneyDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diary.money;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMoneyDiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
